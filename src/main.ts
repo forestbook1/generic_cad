fe1b076afbee6584b53d15c23774ae6d84501519
Gen Morimoto <genunisidep@gmail.com>
2025-03-29 23:33:50 +0900

    first setup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,75 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// set envrionment
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setPixelRatio(Math.min(Math.max(1, window.devicePixelRatio), 2));
+renderer.setClearColor( new THREE.Color( 0xdddddd ) );
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
+
+// add grid helper
+const size = 10;
+const divisions = 10;
+const gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
+// add axes helper
+const axesHelper = new THREE.AxesHelper( 10 );
+scene.add( axesHelper );
+
+
+
+// add some objects
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
+scene.add( line );
+
+
+// onclick event
+window.addEventListener( 'click', (event) => {
+  const mouse = new THREE.Vector2();
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera( mouse, camera );
+  const intersects = raycaster.intersectObjects( scene.children );
+  if ( intersects.length > 0 ) {
+    const intersect = intersects[ 0 ];
+    const point = intersect.point;
+    const sphereGeometry = new THREE.SphereGeometry( 0.15, 32, 32 );
+    const sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+    const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+    sphere.position.copy( point );
+    scene.add( sphere );
+    console.log( 'intersected point: ', point );
+  }
+}
+);
+
+// animate
+function animate() {
+  requestAnimationFrame( animate );
+  controls.update();
+  renderer.render( scene, camera );
+}
+
+animate();
